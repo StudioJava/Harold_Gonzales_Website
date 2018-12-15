07f8b3aed3d65a473b586137e72de1612a4d5bf2
studiojava <ejrojas@icloud.com>
2018-12-14 23:11:20 -0400

    update on links

---
+++ js/aniMain.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+ AOS.init();
+/*$(document).ready(function(){
+
+	// Init ScrollMagic
+	var controller = new ScrollMagic.Controller();
+
+	// build a scene
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '#project01'
+	})
+	.setClassToggle('#project01', 'fade-in') // add class to project01
+	 // this requires a plugin
+	.addTo(controller);
+
+});
+
+TweenMax.from("#logo", 1, {y:20,delay:2,opacity:0});
+*/
+
+  /*$(function() {
+                
+                var documentEl = $(document),
+                    fadeElem = $('.fade-scroll');
+                
+                
+                documentEl.on('scroll', function() {
+                    var currScrollPos = documentEl.scrollTop();
+                    
+                    fadeElem.each(function() {
+                        var $this = $(this),
+                            elemOffsetTop = $this.offset().top;
+                        if (currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/400);
+                    }); 
+                });
+                
+            });*/
---
+++ js/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+	// Init ScrollMagic
+	var controller = new ScrollMagic.Controller();
+
+	// build a scene
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '#project01'
+	})
+	.setClassToggle('#project01', 'fade-in') // add class to project01
+	 // this requires a plugin
+	.addTo(controller);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
